552a29070b0fffdc4986aa511998be26ebe86b39
Wojciech Kapcia <wojtek@tigase.org>
2020-08-18 10:37:38 -0400

    Don't run upgrade-schema when starting up new image (fix NPE); #dockersupport-1

--- nightly/scripts/start.sh
+++ nightly/scripts/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cp -r -n /home/tigase/tigase-server/etc.orig/* /home/tigase/tigase-server/etc/
+
+if grep -q "config-type.*setup" "/home/tigase/tigase-server/etc/config.tdsl" ; then
+  echo "Running in setup mode, skipping schema upgrade...";
+else
+  echo "Upgrading database schema...";
+  ./tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R ${DB_ROOT_USER} -A ${DB_ROOT_PASS}
+fi
+
 echo "Upgrading database schema...";
 ./tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R ${DB_ROOT_USER} -A ${DB_ROOT_PASS}
 echo "Starting Tigase XMPP Server...";
