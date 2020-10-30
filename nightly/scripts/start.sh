7f0402701cad13702ba1e001eaf94598286fb0a6
Wojciech Kapcia <wojtek@tigase.org>
2020-10-30 20:30:04 -0300

    Use tigase.sh directly from distribution package

--- nightly/scripts/start.sh
+++ nightly/scripts/start.sh
@@ -4,9 +4,9 @@ cp -r -n /home/tigase/tigase-server/etc.orig/* /home/tigase/tigase-server/etc/
 if grep -q "config-type.*setup" "/home/tigase/tigase-server/etc/config.tdsl" ; then
   echo "Running in setup mode, skipping schema upgrade...";
 else
-  echo "Upgrading database schema...";
-  ./tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R ${DB_ROOT_USER} -A ${DB_ROOT_PASS} -L ${DB_LOG_LEVEL}
+  echo "Upgrading database schema... x";
+  /home/tigase/tigase-server/scripts/tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R ${DB_ROOT_USER} -A ${DB_ROOT_PASS} -L ${DB_LOG_LEVEL}
 fi
 
 echo "Starting Tigase XMPP Server...";
-./tigase.sh run /home/tigase/tigase-server/etc/tigase.conf
+/home/tigase/tigase-server/scripts/tigase.sh run /home/tigase/tigase-server/etc/tigase.conf
