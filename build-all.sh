2ea113705c8bf11a18a6fa5f6da6ce6650760d73
3nprob <3nprob@3nprob>
2021-03-11 18:22:13 +0900

    Generalize Dockerfile, add 8.1.1

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-LATEST="8.1.0"
+TIGASE_DOCKER_IMAGE="${TIGASE_DOCKER_IMAGE:-tigase/tigase-xmpp-server}"
+LATEST="8.1.1"
 
-for version in nightly 8.0.0 8.1.0 ; do
+for release in nightly 8.1.1-b10861-dist-max 8.1.0-b10857-dist-max 8.0.0-b10083-dist-max; do
+  version=$(echo $release | cut -d'-' -f1)
+  major=$(echo $version | cut -d'.' -f1)
   echo "Building: ${version}"
-#  docker build -t tigase/tigase-xmpp-server:${version} -f ${version}/Dockerfile --no-cache ${version}/
-#  docker push tigase/tigase-xmpp-server:${version}
+  docker build -t ${TIGASE_DOCKER_IMAGE}:${version} -f ./${major}/Dockerfile --build-arg TIGASE_VERSION=${version} --build-arg TIGASE_RELEASE=${release} --no-cache  ./${major}
+  docker push ${TIGASE_DOCKER_IMAGE}:${version}
   if [[ "${LATEST}" == "${version}" ]] ; then
     echo "Pushing ${version} as latest"
-    docker tag tigase/tigase-xmpp-server:${version} tigase/tigase-xmpp-server:latest
-    docker push tigase/tigase-xmpp-server:latest
+    docker tag ${TIGASE_DOCKER_IMAGE}:${version} ${TIGASE_DOCKER_IMAGE}:latest
+    docker push ${TIGASE_DOCKER_IMAGE}:latest
   fi
 done
