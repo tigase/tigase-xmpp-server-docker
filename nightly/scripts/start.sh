73bad67400eb65c07971cf58e9a6fd078ef837d3
Wojciech Kapcia <wojtek@tigase.org>
2021-04-07 20:10:02 +0200

    Check if the database is ready by calling wait-for-it script; server-1243

--- nightly/scripts/start.sh
+++ nightly/scripts/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cp -r -n /home/tigase/tigase-server/etc.orig/* /home/tigase/tigase-server/etc/
 
+if [ -n "${DB_HOST}" ] && [ -n "${DB_PORT}" ] ; then
+  ./scripts/wait-for-it.sh "${DB_HOST}":"${DB_PORT}" -t 30
+fi
 if grep -q "config-type.*setup" "/home/tigase/tigase-server/etc/config.tdsl" ; then
   echo "Running in setup mode, skipping schema upgrade...";
 else
   echo "Upgrading database schema... x";
-  /home/tigase/tigase-server/scripts/tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R ${DB_ROOT_USER} -A ${DB_ROOT_PASS} -L ${DB_LOG_LEVEL}
+  /home/tigase/tigase-server/scripts/tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R "${DB_ROOT_USER}" -A "${DB_ROOT_PASS}" -L "${DB_LOG_LEVEL}"
 fi
 
 echo "Starting Tigase XMPP Server...";
