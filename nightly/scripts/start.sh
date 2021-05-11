ee8c43caa885e7af508e4484440e269808599a8a
Wojciech Kapcia <wojtek@tigase.org>
2021-05-11 14:19:24 +0200

    Slightly corrected start.sh script

--- nightly/scripts/start.sh
+++ nightly/scripts/start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 cp -r -n /home/tigase/tigase-server/etc.orig/* /home/tigase/tigase-server/etc/
 
+echo "Checking if database is already available: ${DB_HOST}:${DB_PORT}";
 if [ -n "${DB_HOST}" ] && [ -n "${DB_PORT}" ] ; then
   ./scripts/wait-for-it.sh "${DB_HOST}":"${DB_PORT}" -t 30
 fi
 if grep -q "config-type.*setup" "/home/tigase/tigase-server/etc/config.tdsl" ; then
   echo "Running in setup mode, skipping schema upgrade...";
 else
-  echo "Upgrading database schema... x";
+  echo "Upgrading database schema...";
   /home/tigase/tigase-server/scripts/tigase.sh upgrade-schema /home/tigase/tigase-server/etc/tigase.conf -R "${DB_ROOT_USER}" -A "${DB_ROOT_PASS}" -L "${DB_LOG_LEVEL}"
 fi
 
